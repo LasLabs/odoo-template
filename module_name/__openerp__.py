89fe1c05e1bf9ac5b822671df05ad23565af79f8
Dave Lasley <dave@laslabs.com>
2016-01-15 17:03:26 -0800

    Move __openerp__ and add more attrs

--- module_name/__openerp__.py
+++ module_name/__openerp__.py
@@ -18,17 +18,36 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
-    'name': '',
-    'version': '8.0.0.0.0',
-    'category': '',
-    'author': "LasLabs",
-    'license': 'AGPL-3',
-    'website': 'https://laslabs.com',
-    'depends': [
+    "name": "Module name",
+    "summary": "Module summary",
+    "version": "8.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://laslabs.com/",
+    "author": "LasLabs",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
     ],
-    'data': [
+    "data": [
+        "security/some_model_security.xml",
+        "security/ir.model.access.csv",
+        "views/assets.xml",
+        "views/report_name.xml",
+        "views/res_partner_view.xml",
+        "wizard/wizard_model_view.xml",
     ],
-    'installable': True,
-    'application': False,
+    "demo": [
+        "demo/res_partner_demo.xml",
+    ],
+    "qweb": [
+        "static/src/xml/module_name.xml",
+    ]
 }
