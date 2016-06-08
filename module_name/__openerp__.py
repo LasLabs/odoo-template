06e8a3145e063953ae89cc13be066f364f38616a
Dave Lasley <dave@laslabs.com>
2016-06-08 09:31:51 -0700

    [FIX] Prefer single to double quotes in manifest

--- module_name/__openerp__.py
+++ module_name/__openerp__.py
@@ -3,34 +3,34 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Module name",
-    "summary": "Module summary",
-    "version": "9.0.1.0.0",
-    "category": "Uncategorized",
-    "website": "https://laslabs.com/",
-    "author": "LasLabs",
-    "license": "AGPL-3",
-    "application": False,
-    "installable": True,
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
+    'name': 'Module name',
+    'summary': 'Module summary',
+    'version': '9.0.1.0.0',
+    'category': 'Uncategorized',
+    'website': 'https://laslabs.com/',
+    'author': 'LasLabs',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
     },
-    "depends": [
-        "base",
+    'depends': [
+        'base',
     ],
-    "data": [
-        "security/some_model_security.xml",
-        "security/ir.model.access.csv",
-        "views/assets.xml",
-        "views/report_name.xml",
-        "views/res_partner_view.xml",
-        "wizard/wizard_model_view.xml",
+    'data': [
+        'security/some_model_security.xml',
+        'security/ir.model.access.csv',
+        'views/assets.xml',
+        'views/report_name.xml',
+        'views/res_partner_view.xml',
+        'wizard/wizard_model_view.xml',
     ],
-    "demo": [
-        "demo/res_partner_demo.xml",
+    'demo': [
+        'demo/res_partner_demo.xml',
     ],
-    "qweb": [
-        "static/src/xml/module_name.xml",
+    'qweb': [
+        'static/src/xml/module_name.xml',
     ]
 }
